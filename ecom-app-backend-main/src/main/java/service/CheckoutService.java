5e0a557d08f9686ae826d116c2e70c5354196853
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-09-05 18:52:47 -0700

    Update CheckoutService.java

--- ecom-app-backend-main/src/main/java/service/CheckoutService.java
+++ ecom-app-backend-main/src/main/java/service/CheckoutService.java
@@ -3,9 +3,7 @@ package service;
 import dto.Purchase;
 import dto.PurchaseResponse;
 
-/**
- * @author <a href="https://github.com/ouhamzalhss"> Lhouceine OUHAMZA </a>
- */
+
 
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
